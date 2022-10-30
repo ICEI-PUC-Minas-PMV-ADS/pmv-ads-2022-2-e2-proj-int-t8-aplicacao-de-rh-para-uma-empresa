5fd9bfced7cdfe93564f1ff2f5aa355042fb0dfd
LuizFernandoFerri <luizfernandorb.fe@gmail.com>
2022-10-30 16:43:31 -0300

    SistemaRH

---
+++ sistemaRH/Models/Atividade.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace sistemaRH.Models
+{
+    [Table("Atividades")]
+    public class Atividade
+    {
+        [Key]
+        public int IdAtividade { get; set; }
+
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage = "Obrigatório informar a descrição da atividade")]
+        public string Descricao { get; set; } = default!;
+
+        [Display(Name = "Nível dificuldade")]
+        [Required(ErrorMessage = "Obrigatório informar nível de dificuldade")]
+        public NivelDificuldade Nivel { get; set; }
+
+    }
+
+    public enum NivelDificuldade { 
+        Fácil,
+        Médio,
+        Díficil
+    }
+
+}
