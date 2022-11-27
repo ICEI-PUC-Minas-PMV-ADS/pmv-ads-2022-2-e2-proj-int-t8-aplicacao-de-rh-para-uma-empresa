00f7ae0de7ba43d6196fc12baf6c6c639f6f8d2c
Luiz Fernando <97108151+LuizFernandorf@users.noreply.github.com>
2022-11-27 13:18:23 -0300

    Novo Commit

---
+++ src/BEMQ/ControleDeContatos/Controllers/ReciboController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeContatos.Controllers
+{
+    public class ReciboController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
