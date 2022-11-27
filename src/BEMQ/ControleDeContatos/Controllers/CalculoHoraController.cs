5be91d90e3bf9498dd9950bcc9a1513ee585c269
Davirsantos <103542765+Davirsantos@users.noreply.github.com>
2022-11-27 19:02:28 -0300

    nova feat

---
+++ src/BEMQ/ControleDeContatos/Controllers/CalculoHoraController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeContatos.Controllers
+{
+    public class CalculoHoraController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
